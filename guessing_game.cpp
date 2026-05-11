30e29be44446fa17555ea0a7dfc106f457e5fc7b
mo8140566-debug <mo8140566@gmail.com>
2026-05-11 18:21:48 +0200

    Added C++ assignment

---
+++ guessing_game.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int chosenNumber = 7;
+    int userGuess = 0;
+
+    vector<int> guesses;
+
+    while(userGuess != chosenNumber) {
+
+        cout << "Enter a number: ";
+        cin >> userGuess;
+
+        guesses.push_back(userGuess);
+
+        if(userGuess > chosenNumber) {
+            cout << "Too high" << endl;
+        }
+
+        else if(userGuess < chosenNumber) {
+            cout << "Too low" << endl;
+        }
+
+        else {
+            cout << "Correct" << endl;
+        }
+    }
+
+    cout << endl;
+    cout << "All guesses entered:" << endl;
+
+    for(int i = 0; i < guesses.size(); i++) {
+        cout << guesses[i] << endl;
+    }
+
+    return 0;
+}
