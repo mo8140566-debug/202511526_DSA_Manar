2ac69765188f8c4c4e224529266cd40da7bfe7a2
mo8140566-debug <mo8140566@gmail.com>
2026-05-14 13:07:37 +0200

    Added C++ assignment

---
+++ create_program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void display()
+{
+    string name = "Manar";
+    cout << "Name: " << name << endl;
+    cout << "Congratulations Manar!" << endl;
+}
+int main()
+{
+    display();
+
+    return 0;
+}
